501ab0c2ec06f0e2c7e2a22ed097d8f618f2fd25
apadros <--->
2025-06-12 10:56:53 +0200

    Basic memory allocation and freeing

---
+++ source/apad_os_types.h
@@ -0,0 +1,5 @@
+#ifndef APAD_OS_TYPES_H
+#define APAD_OS_TYPES_H
+
+
+#endif
---
+++ source/apad_win32.cpp
@@ -0,0 +1,24 @@
+#include "apad_base_types.h"
+#include "apad_debug_error.h"
+
+void* AllocateWin32Memory(ui32 size) {
+  Assert(size > 0);
+  
+  void* mem = VirtualAlloc(NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+  Assert(mem != NULL);
+  
+  return mem;
+}
+
+void FreeWin32Memory(void* mem) {
+  auto ret = VirtualFree(mem, 0, MEM_RELEASE);
+	Assert(ret != 0);
+}
+
+int main() {
+	void* mem = AllocateWin32Memory(1024);
+	
+	FreeWin32Memory(mem);
+	
+	return 0;
+}
