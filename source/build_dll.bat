8c7420482dd0b37329ade89941e6f157926534b5
apadros <--->
2025-06-13 16:51:15 +0200

    Updated build file

---
+++ source/build_dll.bat
@@ -0,0 +1,26 @@
+@echo off
+
+REM Check whether all arguments have been supplied
+if %1.==. ( goto Help )
+
+cls
+
+REM Setup build directory
+if not exist build ( mkdir build )
+del build\* /q
+pushd build
+
+cl /O2 /LD ..\%1
+if exist %1.lib (
+	move *.lib ..\..\bin
+	move *.dll ..\..\bin
+)
+
+popd
+exit /b REM Exit batch script
+
+REM Help message
+:Help
+echo:
+echo Function: Compiles to lib and dll and moves them to the bin folder
+echo:
