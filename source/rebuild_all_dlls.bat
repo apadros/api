1ad5b49caaf1b2bf77fccfe7fa047f9f499f4140
apadros <--->
2025-09-17 13:37:33 +0200

    Minor update to source/rebuild_all_dlls.bat and source/wip.txt

--- source/rebuild_all_dlls.bat
+++ source/rebuild_all_dlls.bat
@@ -11,52 +11,39 @@ del build\* /q
 pushd build
 
 echo:
-echo ###############################################
 echo #################### ERROR ####################
-echo ###############################################
 echo:
 
 cl /O2 /LD /w ..\apad_error.cpp /Feapad_error_%1
 
 echo:
-echo ###############################################
 echo #################### TEMP #####################
-echo ###############################################
 echo:
 
 REM Make temp libs
 cl /O2 /LD /w ..\apad_memory.cpp /Feapad_memory_%1 >> temp.txt
 cl /O2 /LD /w ..\apad_string.cpp /Feapad_string_%1 >> temp.txt
 
-
 echo:
-echo ###############################################
 echo #################### WIN32 ####################
-echo ###############################################
 echo:
 
 cl /O2 /LD /w ..\apad_win32.cpp /Feapad_win32_%1 apad_error_%1.lib apad_memory_%1.lib apad_string_%1.lib
 
 echo:
-echo ################################################
 echo #################### MEMORY ####################
-echo ################################################
 echo:
 
 cl /O2 /LD /w ..\apad_memory.cpp /Feapad_memory_%1 apad_error_%1.lib apad_win32_%1.lib
 
 echo:
-echo ################################################
 echo #################### STRING ####################
-echo ################################################
 echo:
 
 cl /O2 /LD /w ..\apad_string.cpp /Feapad_string_%1 apad_error_%1.lib apad_memory_%1.lib
 
 echo:
-echo ################################################
 echo #################### LOGGING ####################
-echo ################################################
 echo:
 
 cl /O2 /LD /w ..\apad_logging.cpp /Feapad_logging_%1 apad_error_%1.lib apad_memory_%1.lib apad_string_%1.lib
