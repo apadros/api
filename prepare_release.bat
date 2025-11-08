9180942da9547658b825ff51589daa39f9f221f7
apadros <--->
2025-11-08 15:06:28 +0100

    Updates to various batch files to prepare a release package, package itself prepared

---
+++ prepare_release.bat
@@ -0,0 +1,26 @@
+@echo off
+
+REM Check whether all arguments have been supplied
+if %1.==. ( goto Help )
+if %2.==. ( goto Help )
+if %3.==. ( goto Help )
+
+del release\* /q
+
+pushd source\
+call build_all_dlls.bat %1 %2 %3
+popd
+
+echo MOVING...
+echo:
+
+move source\build\* release\
+copy source\*.h release\
+
+exit /b REM Exit batch script
+
+REM Help message
+:Help
+echo:
+echo Usage: prepare_release.bat [version_major_number] [version_minor_number] [version_patch_number]
+echo:
--- source/build_all_dlls.bat
+++ source/build_all_dlls.bat
@@ -2,6 +2,8 @@
 
 REM Check whether all arguments have been supplied
 if %1.==. ( goto Help )
+if %2.==. ( goto Help )
+if %3.==. ( goto Help )
 
 cls
 
@@ -14,55 +16,64 @@ echo:
 echo #################### ERROR ####################
 echo:
 
-cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_error.cpp /Feapad_error_%1
+cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_error.cpp /Feapad_error_v_%1_%2_%3
 
 echo:
 echo #################### TEMP #####################
 echo:
 
 REM Make temp libs
-cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_memory.cpp /Feapad_memory_%1 >> temp.txt
-cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_string.cpp /Feapad_string_%1 >> temp.txt
+cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_memory.cpp /Feapad_memory_v_%1_%2_%3 >> temp.txt
+cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_string.cpp /Feapad_string_v_%1_%2_%3 >> temp.txt
 
 echo:
 echo #################### WIN32 ####################
 echo:
 
-cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_win32.cpp /Feapad_win32_%1 apad_error_%1.lib apad_memory_%1.lib apad_string_%1.lib
+cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_win32.cpp /Feapad_win32_v_%1_%2_%3 apad_error_v_%1_%2_%3.lib apad_memory_v_%1_%2_%3.lib apad_string_v_%1_%2_%3.lib
 
 echo:
 echo #################### MEMORY ####################
 echo:
 
-cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_memory.cpp /Feapad_memory_%1 apad_error_%1.lib apad_win32_%1.lib
+cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_memory.cpp /Feapad_memory_v_%1_%2_%3 apad_error_v_%1_%2_%3.lib apad_win32_v_%1_%2_%3.lib
 
+echo:
 echo #################### FILE ####################
 echo:
 
-cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_file.cpp /Feapad_file_%1 apad_memory_%1.lib apad_win32_%1.lib
+cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_file.cpp /Feapad_file_v_%1_%2_%3 apad_memory_v_%1_%2_%3.lib apad_win32_v_%1_%2_%3.lib
 
 echo:
 echo #################### STRING ####################
 echo:
 
-cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_string.cpp /Feapad_string_%1 apad_error_%1.lib apad_memory_%1.lib
+cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_string.cpp /Feapad_string_v_%1_%2_%3 apad_error_v_%1_%2_%3.lib apad_memory_v_%1_%2_%3.lib
 
 echo:
 echo #################### LOGGING ####################
 echo:
 
-cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_logging.cpp /Feapad_logging_%1 apad_error_%1.lib apad_memory_%1.lib apad_string_%1.lib
+cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_logging.cpp /Feapad_logging_v_%1_%2_%3 apad_error_v_%1_%2_%3.lib apad_memory_v_%1_%2_%3.lib apad_string_v_%1_%2_%3.lib
 
 echo:
 echo #################### TIME ####################
 echo:
 
-cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_time.cpp /Feapad_time_%1 apad_error_%1.lib apad_string_%1.lib
+cl /nologo /DAPAD_DLL /O2 /LD /w ..\apad_time.cpp /Feapad_time_v_%1_%2_%3 apad_error_v_%1_%2_%3.lib apad_string_v_%1_%2_%3.lib
 
 
 del temp.txt
 del *.obj
 del *.exp
+
+echo:
+echo RENAMING...
+echo:
+
+REM For some reason, supplying the compiler with vx.y.z instead of v_x_y_z creates errors
+ren *v_%1_%2_%3.lib *v%1.%2.%3.lib
+ren *v_%1_%2_%3.dll *v%1.%2.%3.dll
 
 popd
 exit /b REM Exit batch script
@@ -70,5 +81,5 @@ exit /b REM Exit batch script
 REM Help message
 :Help
 echo:
-echo Usage: build_dll.bat version(vx.y.z)
+echo Usage: build_dll.bat [version_major_number] [version_minor_number] [version_patch_number]
 echo:
