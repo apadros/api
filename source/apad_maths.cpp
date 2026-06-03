2bc78a8d31f849f0cc885d5a5f3679944ef9abe0
apadros <apadros@bluewin.ch>
2026-06-03 13:30:21 +0200

    [MATHS, WIN32_GUI] Moved rectangle and point structs to apad_maths.h API

--- source/apad_maths.cpp
+++ source/apad_maths.cpp
@@ -1,6 +1,7 @@
 #include "apad_base_types.h"
+#include "apad_intrinsics.h"
 
-bool Overlap(ui16 x0, ui16 y0, ui16 left1, ui16 bottom1, ui16 width1, ui16 height1) {
+dll_export program_external bool Overlap(ui16 x0, ui16 y0, ui16 left1, ui16 bottom1, ui16 width1, ui16 height1) {
 	return x0 >= left1 && x0 <= left1 + width1 &&
 				 y0 >= bottom1 && y0 <= bottom1 + height1;
 }
