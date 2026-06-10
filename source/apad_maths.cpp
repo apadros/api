286bd8a1cc7139d82a343a25dd9c2a1506404471
apadros <apadros@bluewin.ch>
2026-06-10 16:16:30 +0200

    [MATHS] Updated point struct to use f32 instead of ui16, same with Overlap()

--- source/apad_maths.cpp
+++ source/apad_maths.cpp
@@ -2,7 +2,7 @@
 #include "apad_intrinsics.h"
 #include "apad_maths.h"
 
-dll_export program_external bool Overlap(ui16 x0, ui16 y0, ui16 left1, ui16 bottom1, ui16 width1, ui16 height1) {
+dll_export program_external bool Overlap(f32 x0, f32 y0, f32 left1, f32 bottom1, f32 width1, f32 height1) {
 	return x0 >= left1 && x0 <= left1 + width1 &&
 				 y0 >= bottom1 && y0 <= bottom1 + height1;
 }
